c58c71d9a654b5664a3414988caa21922ed0ffff
ollevche <ollevche@gmail.com>
2021-06-03 23:37:31 +0300

    prettify main package

---
+++ cmd/transferer/export.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/rs/zerolog/log"
+
+func runClickHouseExport(_ string) error {
+	log.Fatal().Msg("TO BE DONE")
+	return nil
+}
+
+func runVictoriaMetricsExport(_ string) error {
+	log.Fatal().Msg("TO BE DONE")
+	return nil
+}
--- cmd/transferer/main.go
+++ cmd/transferer/main.go
@@ -8,41 +8,53 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func init() {
-	log.Logger = log.
-		Output(zerolog.ConsoleWriter{Out: os.Stdout}).
-		With().Caller().Logger()
-}
-
-var (
-	transferer = kingpin.New("pmm-transferer", "Percona PMM Transferer")
-
-	// TODO: is it possible to get URL from PMM Server? Get metrics indirectly through PMM Server?
-	chURL = transferer.Flag("ch_url", "ClickHouse connection URL.").String()
-	vmURL = transferer.Flag("vm_url", "VictoriaMetrics connection URL.").String()
-
-	exportCmd = transferer.Command("export", "Export metrics from PMM Server to file.")
-
-	importCmd = transferer.Command("import", "Import metrics to PMM Server from file.")
-)
-
 func main() {
+	var (
+		transferer = kingpin.New("pmm-transferer", "Percona PMM Transferer")
+
+		clickHouseURL      = transferer.Flag("click_house_url", "ClickHouse connection string").String()
+		victoriaMetricsURL = transferer.Flag("victoria_metrics_url", "VictoriaMetrics connection string").String()
+		enableVerboseMode  = transferer.Flag("verbose_mode", "Enable verbose mode").Short('v').Bool()
+
+		exportCmd = transferer.Command("export", "Export PMM Server metrics to dump file")
+
+		importCmd = transferer.Command("import", "Import PMM Server metrics from dump file")
+	)
+
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout})
+	if *enableVerboseMode {
+		log.Logger = log.Logger.
+			Level(zerolog.DebugLevel).
+			With().Caller().
+			Logger()
+	}
+
 	cmd, err := transferer.DefaultEnvars().Parse(os.Args[1:])
 	if err != nil {
 		log.Fatal().Msgf("Error parsing parameters: %s", err.Error())
 	}
 
-	if chURL != nil {
-		log.Info().Str("ch_url", *chURL).Msg("ClickHouse URL supplied")
+	if *clickHouseURL == "" && *victoriaMetricsURL == "" {
+		log.Fatal().Msg("Please, specify at least one data source via connection string")
 	}
 
-	if vmURL != nil {
-		log.Info().Str("vm_url", *vmURL).Msg("VictoriaMetrics URL supplied")
+	switch cmd {
+	case exportCmd.FullCommand():
+		if *clickHouseURL != "" {
+			err = runClickHouseExport(*clickHouseURL)
+			if err != nil {
+				log.Fatal().Msgf("Failed to run click house export: %v", err)
+			}
+		}
+		if *victoriaMetricsURL != "" {
+			err = runVictoriaMetricsExport(*victoriaMetricsURL)
+			if err != nil {
+				log.Fatal().Msgf("Failed to run victoria metrics export: %v", err)
+			}
+		}
+	case importCmd.FullCommand():
+		log.Fatal().Msg("TO BE DONE")
+	default:
+		log.Fatal().Msgf("Undefined command found: %s", cmd)
 	}
-
-	log.Info().
-		Str("current_cmd", cmd).
-		Str("export", exportCmd.FullCommand()).
-		Str("import", importCmd.FullCommand()).
-		Msg("Run successfully!")
 }
