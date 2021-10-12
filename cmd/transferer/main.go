a21be50921862b8a327ac5a3a56c7a98db0f2830
Andrii Dema <a.dema@jazzserve.com>
2021-10-12 15:49:09 +0300

    SE-56: Warn user when filters for QAN metrics are specified and are not specified for core metrics. And vice versa
    https://jira.percona.com/browse/SE-56

--- cmd/transferer/main.go
+++ cmd/transferer/main.go
@@ -115,6 +115,14 @@ func main() {
 			log.Fatal().Msg("Please, specify at least one data source")
 		}
 
+		if *dumpQAN && *dumpCore && len(*instances) == 0 {
+			if *where == "" && (*tsSelector != "" || len(*dashboards) > 0) {
+				log.Warn().Msg("Filter for QAN found, but not for core dump. Core metrics for all metrics would be exported")
+			} else if *where != "" && *tsSelector == "" && len(*dashboards) == 0 {
+				log.Warn().Msg("Filter for core dump found, but not for QAN. QAN metrics for all metrics would be exported")
+			}
+		}
+
 		var sources []dump.Source
 
 		pmmConfig, err := getPMMConfig(*pmmURL, *victoriaMetricsURL, *clickHouseURL)
