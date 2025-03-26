7ff1f4f34132301fe9a4653235f8ea48f7ba1642
pepsiqqq <pepsiqqfanta@gmail.com>
2025-03-26 17:50:18 +0200

    Revert

--- internal/test/deployment/pmm.go
+++ internal/test/deployment/pmm.go
@@ -178,7 +178,7 @@ func (p *PMM) ClickhouseURL() string {
 	if strings.Contains(u.Host, ":") {
 		u.Host = u.Host[0:strings.Index(u.Host, ":")]
 	}
-	u.Host += ":" + *p.clickhouseHTTPPort
+	u.Host += ":" + *p.clickhousePort
 
 	return u.String()
 }
