974d78855af622d229bbbdb699bc50f39dd612d3
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2025-12-17 14:53:04 +0100

    PMM-14577 Fix original logic.

--- pkg/grafana/templating/interpolate.go
+++ pkg/grafana/templating/interpolate.go
@@ -20,7 +20,6 @@ import (
 	"regexp"
 	"strings"
 	"time"
-
 	"github.com/pkg/errors"
 )
 
@@ -37,11 +36,13 @@ func FormatVar(format VariableFormat, input []string) (string, error) {
 }
 
 func InterpolateQuery(query string, from time.Time, to time.Time, vars []TemplatingVariable) (string, error) {
-	if query == "" {
-		return "", nil
-	}
-	query = ApplyMacros(query, from, to)
-
+       if query == "" {
+	       return "", nil
+       }
+       query, err := ApplyMacros(query, TimeRange{From: from, To: to})
+       if err != nil {
+	       return "", errors.Wrap(err, "failed to apply macros")
+       }
 	for _, v := range vars {
 		str, err := v.Interpolate("")
 		if err != nil {
--- pkg/grafana/templating/macros.go
+++ pkg/grafana/templating/macros.go
@@ -15,20 +15,126 @@
 package templating
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 	"time"
 )
 
-// ApplyMacros replaces supported Grafana macros in the query string.
-func ApplyMacros(query string, from, to time.Time) string {
-	duration := to.Sub(from)
-	query = strings.ReplaceAll(query, "$__range", duration.String())
-	query = strings.ReplaceAll(query, "$__from", formatUnix(from))
-	query = strings.ReplaceAll(query, "$__to", formatUnix(to))
-	// Add more macro replacements as needed
-	return query
-}
-
 func formatUnix(t time.Time) string {
 	return strings.TrimSuffix(strings.TrimPrefix(t.Format("2006-01-02T15:04:05Z07:00"), "T"), "Z")
+}
+
+type TimeRange struct {
+	From time.Time
+	To   time.Time
+}
+
+func FromMacro(inputString string, timeRange TimeRange) (string, error) {
+	res, err := applyMacro("$$from", inputString, func(_ string, args []string) (string, error) {
+		 return expandTimeMacro(timeRange.From, args)
+	})
+	return res, err
+}
+
+func ToMacro(inputString string, timeRange TimeRange) (string, error) {
+	res, err := applyMacro("$$to", inputString, func(_ string, args []string) (string, error) {
+		 return expandTimeMacro(timeRange.To, args)
+	})
+	return res, err
+}
+
+func expandTimeMacro(t time.Time, args []string) (string, error) {
+	if len(args) < 1 || args[0] == "" {
+		return fmt.Sprintf("%d", t.UnixMilli()), nil
+	}
+	if args[0] == "date" {
+		if len(args) < 2 || args[1] == ":iso" {
+			return t.Format("2006-01-02T15:04:05.999Z"), nil
+		}
+	}
+	format := strings.TrimPrefix(strings.Join(args, ","), "date:")
+	if format == "iso" {
+		return t.Format("2006-01-02T15:04:05.999Z"), nil
+	}
+	format = strings.ReplaceAll(format, "YYYY", "2006")
+	format = strings.ReplaceAll(format, "YY", "06")
+
+	format = strings.ReplaceAll(format, "MMMM", "January")
+	format = strings.ReplaceAll(format, "MMM", "Jan")
+	format = strings.ReplaceAll(format, "MM", "01")
+	format = strings.ReplaceAll(format, "M", "1")
+
+	format = strings.ReplaceAll(format, "DD", "02")
+	format = strings.ReplaceAll(format, "D", "2")
+
+	format = strings.ReplaceAll(format, "hh", "03")
+	format = strings.ReplaceAll(format, "h", "3")
+	format = strings.ReplaceAll(format, "HH", "15")
+
+	format = strings.ReplaceAll(format, "mm", "04")
+	format = strings.ReplaceAll(format, "m", "4")
+
+	format = strings.ReplaceAll(format, "ss", "05")
+	format = strings.ReplaceAll(format, "s", "5")
+
+	format = strings.ReplaceAll(format, "S", "0")
+
+	format = strings.ReplaceAll(format, "A", "PM")
+
+	format = strings.ReplaceAll(format, "zz", "MST")
+	format = strings.ReplaceAll(format, "z", "MST")
+
+	format = strings.ReplaceAll(format, "dddd", "Monday")
+	format = strings.ReplaceAll(format, "ddd", "Mon")
+
+	return t.Format(format), nil
+}
+
+func ApplyMacros(input string, timeRange TimeRange) (string, error) {
+       input, err := FromMacro(input, timeRange)
+       if err != nil {
+	       return input, err
+       }
+       input, err = ToMacro(input, timeRange)
+       if err != nil {
+	       return input, err
+       }
+       return input, nil
+}
+
+type macroFunc func(string, []string) (string, error)
+
+func getMatches(macroName, input string) ([][]string, error) {
+	macroRegex := fmt.Sprintf("\\$__%s\\b(?:\\((.*?)\\))?", macroName) // regular macro syntax
+	if strings.HasPrefix(macroName, "$$") {                            // prefix $$ is used to denote macro from frontend or grafana global variable
+		macroRegex = fmt.Sprintf("\\${__%s:?(.*?)}", strings.TrimPrefix(macroName, "$$"))
+	}
+	rgx, err := regexp.Compile(macroRegex)
+	if err != nil {
+		return nil, err
+	}
+	return rgx.FindAllStringSubmatch(input, -1), nil
+}
+
+func applyMacro(macroKey string, queryString string, macro macroFunc) (string, error) {
+	matches, err := getMatches(macroKey, queryString)
+	if err != nil {
+		return queryString, err
+	}
+	for _, match := range matches {
+		if len(match) == 0 {
+			continue
+		}
+		args := []string{}
+		if len(match) > 1 {
+			args = strings.Split(match[1], ",")
+		}
+		res, err := macro(queryString, args)
+		if err != nil {
+			return queryString, err
+		}
+		queryString = strings.ReplaceAll(queryString, match[0], res)
+	}
+	return strings.TrimSpace(queryString), nil
 }
