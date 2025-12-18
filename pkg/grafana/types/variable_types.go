f81337c9aabcaebe9ffdf3313c76dffcc87dd513
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2025-12-18 11:13:37 +0100

    PMM-14577 Grafana code license and lint.

--- pkg/grafana/types/variable_types.go
+++ pkg/grafana/types/variable_types.go
@@ -1,4 +1,5 @@
-// Copyright 2023 Percona LLC
+// Portions of this file are derived from the Grafana project (https://github.com/grafana/grafana)
+// and are licensed under the Apache License, Version 2.0.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
