7a1cfe9801efd4fd58b7d06e65f181b050114d75
Jiří Čtvrtka <62988319+JiriCtvrtka@users.noreply.github.com>
2026-01-07 15:38:13 +0100

    PMM-14577 Remove Grafana and Grafana SDK from dependencies. (#345)
    * PMM-14577 Bumps.
    
    * PMM-14577 Remove Grafana.
    
    * PMM-14577 Remove Grafana SDK.
    
    * PMM-14577 Put same logic back (changed during removing Grafana).
    
    * PMM-14577 Merge conflicts.
    
    * PMM-14577 Fix license headers.
    
    * PMM-14577 Lint.
    
    * PMM-14577 Revert tools update.
    
    * PMM-14577 Fix for revert.
    
    * PMM-14577 Fix original logic.
    
    * PMM-14577 Lint.
    
    * PMM-14577 Fix.
    
    * PMM-14577 Fix.
    
    * PMM-14577 Lint.
    
    * PMM-14577 Lint, format.
    
    * PMM-14577 Add back changes in tools.
    
    * PMM-14577 Lint.
    
    * trigger
    
    * PMM-14577 Grafana code license and lint.
    
    * PMM-14577 Ignore.
    
    * PMM-14577 Format.
    
    * PMM-14577 Grafana license.
    
    * PMM-14577 Try merge licenses.
    
    * PMM-14577 Fix license-eye.
    
    * PMM-14577 Missing line.

---
+++ pkg/grafana/types/variable_types.go
@@ -0,0 +1,91 @@
+// Copyright 2023 Percona LLC
+//
+// Portions of this file are derived from the Grafana project (https://github.com/grafana/grafana)
+// and are licensed under the Apache License, Version 2.0.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import "encoding/json"
+
+// VariableType is the type of a dashboard variable.
+type VariableType string
+
+const (
+	VariableTypeQuery      VariableType = "query"
+	VariableTypeCustom     VariableType = "custom"
+	VariableTypeConstant   VariableType = "constant"
+	VariableTypeAdhoc      VariableType = "adhoc"
+	VariableTypeDatasource VariableType = "datasource"
+	VariableTypeInterval   VariableType = "interval"
+)
+
+// VariableText can be a string or an array of strings in JSON.
+type VariableText []string
+
+func (t *VariableText) UnmarshalJSON(data []byte) error {
+	// Try to unmarshal as a string
+	var s string
+	if err := json.Unmarshal(data, &s); err == nil {
+		*t = []string{s}
+		return nil
+	}
+	// Try to unmarshal as an array of strings
+	var arr []string
+	if err := json.Unmarshal(data, &arr); err == nil {
+		*t = arr
+		return nil
+	}
+	return json.Unmarshal(data, (*[]string)(t))
+}
+
+// VariableOption represents an option for a dashboard variable.
+type VariableOption struct {
+	Text  VariableText `json:"text"`
+	Value interface{}  `json:"value"`
+}
+
+// VariableSort represents sorting options for a variable.
+type VariableSort struct {
+	Type int  `json:"type"`
+	Desc bool `json:"desc"`
+}
+
+// UnmarshalJSON allows VariableSort to be unmarshaled from either a number or an object.
+func (s *VariableSort) UnmarshalJSON(data []byte) error {
+	// Try to unmarshal as a number (Grafana sometimes encodes as int)
+	var asInt int
+	if err := json.Unmarshal(data, &asInt); err == nil {
+		s.Type = asInt
+		s.Desc = false
+		return nil
+	}
+	// Try to unmarshal as a struct
+	var asStruct struct {
+		Type int  `json:"type"`
+		Desc bool `json:"desc"`
+	}
+	if err := json.Unmarshal(data, &asStruct); err != nil {
+		return err
+	}
+	s.Type = asStruct.Type
+	s.Desc = asStruct.Desc
+	return nil
+}
+
+// VariableHide represents hide options for a variable.
+type VariableHide int
+
+// VariableRefresh represents refresh options for a variable.
+type VariableRefresh int
