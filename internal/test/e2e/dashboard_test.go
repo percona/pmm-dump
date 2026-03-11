66657491f53c4a991fbb6ee359b59be2d8df0a04
Jiří Čtvrtka <j.ctvrtka@gmail.com>
2026-03-11 13:55:17 +0100

    PMM-14829 Debug.

--- internal/test/e2e/dashboard_test.go
+++ internal/test/e2e/dashboard_test.go
@@ -44,6 +44,7 @@ func TestDashboard(t *testing.T) {
 	for _, name := range names {
 		t.Run(name, func(t *testing.T) {
 			// this one failing, so it is skipped for now
+			pmm.Log(name)
 			if name == "TestDashboard/PMM_HA_Health_Overview" {
 				t.Skip()
 			}
